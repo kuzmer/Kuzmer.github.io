64761255301563f94fcbf01bbcaa92d18d1f9814
m.karpov <m.karpov@usd.systems>
2022-12-17 15:55:21 +0200

    upd animation

---
+++ js/main.js
@@ -0,0 +1,49 @@
+var LinksPage = LinksPage || {};
+
+LinksPage.Data = 
+{
+	EnableVibro: true,
+	SelectedLinkIndex: -1
+};
+
+LinksPage.OnloadPage = function()
+{
+	setTimeout(LinksPage.ChangeLinkDecorate, 2000);
+}
+
+LinksPage.ChangeLinkDecorate = function()
+{
+	let links = document.body.querySelector(".intro__nav").querySelectorAll("a");
+	
+	LinksPage.Data.SelectedLinkIndex +=1;
+	
+	if(links.length <= LinksPage.Data.SelectedLinkIndex )
+	{
+		LinksPage.Data.SelectedLinkIndex = 0;
+	}
+	
+	
+	links.forEach((el, index) => {
+		if(index == LinksPage.Data.SelectedLinkIndex)
+		{
+			el.classList.add("glow-link")
+		}
+		else
+		{ 
+			el.classList.remove("glow-link");
+		}
+});
+
+setTimeout(LinksPage.VibroEffect, 1);
+setTimeout(LinksPage.ChangeLinkDecorate, 4000);
+
+}
+
+LinksPage.VibroEffect = function()
+{
+	if(LinksPage.Data)
+	{
+		window.navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]);		
+	}
+	
+}
